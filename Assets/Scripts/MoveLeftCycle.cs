366e028dacb9315663fb210fe84df49259bc0b2a
barracius <rjgonzalez6@gmail.com>
2020-10-13 16:59:03 -0300

    First commit

---
+++ Assets/Scripts/MoveLeftCycle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftCycle : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    [SerializeField] private float leftWayPointX = -8f, rightWayPointX = 8f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var transform1 = transform;
+        var position = transform1.position;
+        position = new Vector2(position.x + moveSpeed * Time.deltaTime, position.y);
+        transform1.position = position;
+        if (!(transform.position.x < leftWayPointX)) return;
+        var transform2 = transform;
+        transform2.position = new Vector2(rightWayPointX, transform2.position.y);
+    }
+}
---
+++ Assets/Scripts/ObstacleControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleControl : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = -5f;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var transform1 = transform;
+        var position = transform1.position;
+        position = new Vector2(position.x + moveSpeed * Time.deltaTime, position.y);
+        transform1.position = position;
+        if (transform.position.x < -13f)
+            Destroy(gameObject);
+        if (Input.GetKey("up"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name.Equals("Wolfie"))
+            GameControl.instance.WolfieCrushes();
+    }
+    
+}
