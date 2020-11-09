854da8501be9810f8c191ebb0d1a29429126103d
barracius <rjgonzalez6@gmail.com>
2020-11-09 04:10:35 -0300

    WorldMap, Stars system, replay and game flow Done

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl Instance;
 
-    [SerializeField] private GameObject restartButton;
+    [SerializeField] private GameObject panel;
 
     [SerializeField] private Text highScoreText;
 
@@ -22,6 +23,9 @@ public class GameControl : MonoBehaviour
     [SerializeField] private float spawnRate = 2f;
     
     [SerializeField] private float timeToBoost = 5f;
+    [SerializeField] private const int OneStarScore = 20;
+    [SerializeField] private const int TwoStarScore = 40;
+    [SerializeField] private const int ThreeStarScore = 60;
 
     private float _nextBoost, _nextScoreIncrease,  _nextSpawn;
 
@@ -30,31 +34,43 @@ public class GameControl : MonoBehaviour
     internal static bool GameStopped;
     
     public List<GameObject> obstaclesInScene;
+    public int numberOfStage;
+    
+    private Transform _achievedLeftStar;
+    private Transform _achievedMidStar;
+    private Transform _achievedRightStar;
+    private Transform _emptyLeftStar;
+    private Transform _emptyMidStar;
+    private Transform _emptyRightStar;
+    
 
     private void Start()
     {
         if (Instance == null) Instance = this;
         else if (Instance != this) Destroy(gameObject);
-        restartButton.SetActive(false);
+        panel.SetActive(false);
         _yourScore = 0;
         GameStopped = false;
         Time.timeScale = 1f;
         // RESET HIGH SCORE
         //PlayerPrefs.SetInt("highScore", 0);
-        _highScore = PlayerPrefs.GetInt("level1Score");
+        _highScore = PlayerPrefs.GetInt("level" + numberOfStage + "Score", _yourScore);
         _nextSpawn = Time.time + spawnRate;
         _nextBoost = Time.unscaledTime + timeToBoost;
+        
+        _achievedLeftStar = panel.transform.Find("A Left Star");
+        _achievedMidStar = panel.transform.Find("A Mid Star");
+        _achievedRightStar = panel.transform.Find("A Right Star");
+        _emptyLeftStar = panel.transform.Find("E Left Star");
+        _emptyMidStar = panel.transform.Find("E Mid Star");
+        _emptyRightStar = panel.transform.Find("E Right Star");
     }
     
     private void Update()
     {
         if (!GameStopped) IncreaseYourScore();
         highScoreText.text = "High Score: " + _highScore;
-        if (_yourScore >= 30)
-        {
-            yourScoreText.text = "Your Score: " + _yourScore + "\nCongratulations! You unlocked the next stage";
-        }
-        else yourScoreText.text = "Your Score: " + _yourScore;
+        yourScoreText.text = "Your Score: " + _yourScore;
 
         if (Time.time > _nextSpawn) SpawnObstacle();
         if (Time.unscaledTime > _nextBoost && !GameStopped) BoostTime();
@@ -62,10 +78,13 @@ public class GameControl : MonoBehaviour
 
     public void Loss()
     {
-        if (_yourScore > _highScore) PlayerPrefs.SetInt("level1Score", _yourScore);
+        if (_yourScore > _highScore) PlayerPrefs.SetInt("level" + numberOfStage + "Score", _yourScore);
+        AdjustTotalStars();
+        
         Time.timeScale = 0;
         GameStopped = true;
-        restartButton.SetActive(true);
+        panel.SetActive(true);
+        ShowCurrentStars();
     }
 
     private void SpawnObstacle()
@@ -92,6 +111,60 @@ public class GameControl : MonoBehaviour
 
     public void RestartGame()
     {
-        SceneManager.LoadScene("1");
+        SceneManager.LoadScene(numberOfStage.ToString());
+    }
+
+    public void OnCrossPressed()
+    {
+        SceneManager.LoadScene("WorldMap");
+    }
+
+    private void AdjustTotalStars()
+    {
+        switch (PlayerPrefs.GetInt("level" + numberOfStage + "Stars", 0))
+        {
+            case 3:
+                break;
+            case 2:
+                if (_yourScore >= ThreeStarScore)
+                {
+                    PlayerPrefs.SetInt("level" + numberOfStage + "Stars", 3);
+                }
+                break;
+            case 1:
+                if (_yourScore >= ThreeStarScore)
+                {
+                    PlayerPrefs.SetInt("level" + numberOfStage + "Stars", 3);
+                }
+                else if (_yourScore >= TwoStarScore)
+                {
+                    PlayerPrefs.SetInt("level" + numberOfStage + "Stars", 2);
+                }
+                break;
+            case 0:
+                if (_yourScore >= ThreeStarScore)
+                {
+                    PlayerPrefs.SetInt("level" + numberOfStage + "Stars", 3);
+                }
+                else if (_yourScore >= TwoStarScore)
+                {
+                    PlayerPrefs.SetInt("level" + numberOfStage + "Stars", 2);
+                }
+                else if (_yourScore >= OneStarScore)
+                {
+                    PlayerPrefs.SetInt("level" + numberOfStage + "Stars", 1);
+                }
+                break;
+        }
+    }
+
+    private void ShowCurrentStars()
+    {
+        _achievedLeftStar.gameObject.SetActive(false);
+        _achievedMidStar.gameObject.SetActive(false);
+        _achievedRightStar.gameObject.SetActive(false);
+        if (_yourScore >= OneStarScore) _achievedLeftStar.gameObject.SetActive(true);
+        if (_yourScore >= TwoStarScore) _achievedMidStar.gameObject.SetActive(true);
+        if (_yourScore >= ThreeStarScore) _achievedRightStar.gameObject.SetActive(true);
     }
 }
--- Assets/Scripts/World  Map Scripts/StageController.cs
+++ Assets/Scripts/World  Map Scripts/StageController.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 namespace World__Map_Scripts
 {
     public class StageController : MonoBehaviour
     {
-        [SerializeField] private string sceneToGo;
         [SerializeField] public int numberOfStage;
         private Transform _locked;
         private Transform _unlocked;
         private int _stars;
+        private int _prevStageStars;
         private Transform _achievedLeftStar;
         private Transform _achievedMidStar;
         private Transform _achievedRightStar;
@@ -28,34 +29,72 @@ namespace World__Map_Scripts
             _achievedMidStar = _unlocked.Find("A Mid Star");
             _achievedRightStar = _unlocked.Find("A Right Star");
             _emptyLeftStar = _unlocked.Find("E Left Star");
-            _emptyMidStar = _unlocked.Find("E Left Star");
-            _emptyRightStar = _unlocked.Find("E Left Star");
-            _unlocked.gameObject.SetActive(false);
-            
+            _emptyMidStar = _unlocked.Find("E Mid Star");
+            _emptyRightStar = _unlocked.Find("E Right Star");
+
+            if (CheckIfUnlocked())
+            {
+                _locked.gameObject.SetActive(false);
+                _unlocked.gameObject.SetActive(true);
+                CalculateLevelStars();
+                _isLocked = false;
+            }
+            else
+            {
+                _unlocked.gameObject.SetActive(false);
+                _isLocked = true;
+            }
         }
 
         private void CalculateLevelStars()
         {
-            _stars = PlayerPrefs.GetInt("level" + numberOfStage + "Stars");
+            _stars = PlayerPrefs.GetInt("level" + numberOfStage + "Stars", 0);
             switch (_stars)
             {
                 case 1:
                     _achievedLeftStar.gameObject.SetActive(true);
                     _emptyMidStar.gameObject.SetActive(true);
                     _emptyRightStar.gameObject.SetActive(true);
+                    
+                    _emptyLeftStar.gameObject.SetActive(false);
+                    _achievedMidStar.gameObject.SetActive(false);
+                    _achievedRightStar.gameObject.SetActive(false);
                     break;
                 case 2:
                     _achievedLeftStar.gameObject.SetActive(true);
                     _achievedMidStar.gameObject.SetActive(true);
                     _emptyRightStar.gameObject.SetActive(true);
+                    
+                    _emptyLeftStar.gameObject.SetActive(false);
+                    _emptyMidStar.gameObject.SetActive(false);
+                    _achievedRightStar.gameObject.SetActive(false);
                     break;
                 case 3:
                     _achievedLeftStar.gameObject.SetActive(true);
                     _achievedMidStar.gameObject.SetActive(true);
                     _achievedRightStar.gameObject.SetActive(true);
+                    
+                    _emptyLeftStar.gameObject.SetActive(false);
+                    _emptyMidStar.gameObject.SetActive(false);
+                    _emptyRightStar.gameObject.SetActive(false);
                     break;
             }
             
+        }
+
+        private bool CheckIfUnlocked()
+        {
+            if (numberOfStage == 1) return true;
+            _prevStageStars = PlayerPrefs.GetInt("level" + (numberOfStage - 1) + "Stars", 0);
+            return _prevStageStars != 0;
+        }
+
+        public void OnClickStage()
+        {
+            if (!_isLocked)
+            {
+                SceneManager.LoadScene(numberOfStage.ToString());
+            }
         }
     }
 }
